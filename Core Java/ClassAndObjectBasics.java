6f9a9be5b0b96774175b9973428f9d21e2ee1b91
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2026-05-03 23:52:26 +0530

    Update ClassAndObjectBasics.java

--- Core Java/ClassAndObjectBasics.java
+++ Core Java/ClassAndObjectBasics.java
@@ -1,1 +1,28 @@
+// Class definition
+class Calculator {
+    
+    // Method to perform addition
+    int add(int a, int b) {
+        int r = a + b;   // store result
+        return r;        // return result
+    }
+}
 
+// Main class (same name as file)
+public class ClassAndObjectBasics {
+    public static void main(String[] args) {
+        
+        // Creating variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Creating object
+        Calculator calc = new Calculator();
+        
+        // Calling method
+        int result = calc.add(num1, num2);
+        
+        // Printing result
+        System.out.println("Addition Result: " + result);
+    }
+}
